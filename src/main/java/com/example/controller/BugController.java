aff4cafc0fb63ea7038e503f4cfc53c22234f1ba
Nandan1981 <Nandan1981@gmail.com>
2019-04-20 12:09:56 +0530

    Short description of commit
    Longer explanation of the motivation for the change
    
    Fixes-Bug: Enter bug-id or delete line
    Implements-Requirement: Enter requirement-id or delete line
    
    	First commit to test jenkins build

---
+++ src/main/java/com/example/controller/BugController.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Nandan
+ *
+ */
+@Controller
+public class BugController {
+	
+	private String index = "index";
+	
+	@RequestMapping(value="/welcome")
+	public String welcomeController() {
+		
+System.out.println("in welcome");
+		return index;
+	}
+
+}
